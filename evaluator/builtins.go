637daa3656360c97b12cdcea4d4f4b392e8482b8
cosmonawt <cosmonawt@protonmail.ch>
2020-01-04 23:50:41 +0100

    add builtin `print` function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -14,6 +17,14 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
